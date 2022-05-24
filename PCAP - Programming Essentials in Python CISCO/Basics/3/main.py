92c3d820e1a3acdf3cdac6e86373368cd2caf478
egiam <ezegiampaoli@gmail.com>
2022-05-24 10:02:00 -0300

    update

---
+++ PCAP - Programming Essentials in Python CISCO/Basics/3/main.py
@@ -0,0 +1,18 @@
+﻿word_without_vowels = ""
+user_word = input("Ingrese la palabra: ").upper()
+vocales = ["A", "E", "I", "O", "U"]
+
+# Indicar al usuario que ingrese una palabra
+# y asignarla a la variable user_word.
+
+
+for letter in user_word:
+   # Completa el cuerpo del bucle.
+   if letter in vocales:
+       continue
+   else:
+       word_without_vowels += letter
+       
+print(word_without_vowels)
+
+# Imprimir la palabra asignada a word_without_vowels.
---
+++ PCAP - Programming Essentials in Python CISCO/Basics/3/main_piramide.py
@@ -0,0 +1,13 @@
+﻿blocks = int(input("Ingresa el número de bloques: "))
+
+#
+# Escribe tu código aquí.
+height = 1
+while blocks > 0:
+    if blocks < height:
+        pass
+    else:
+	    height += 1;
+    blocks -= height;
+
+print("La altura de la pirámide:", height)
