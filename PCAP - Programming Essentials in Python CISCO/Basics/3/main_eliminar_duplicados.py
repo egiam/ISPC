9b002bcc3fec2fcc18169d43cad199c2ef947c3e
egiam <ezegiampaoli@gmail.com>
2022-05-25 11:40:35 -0300

    Examen de Ingles y Finalizacion del 3er modulo de PCAP

---
+++ PCAP - Programming Essentials in Python CISCO/Basics/3/main_eliminar_duplicados.py
@@ -0,0 +1,9 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+#
+# Escribe tu código aquí.
+my_list = set(my_list)
+print(my_list)
+my_list = list(my_list)
+#
+print("La lista con elementos únicos:")
+print(my_list)
---
+++ PCAP - Programming Essentials in Python CISCO/Basics/3/main_sin_vocales.py
@@ -0,0 +1,26 @@
+﻿# Declaracion de la variable palabras sin vocales
+word_without_vowels = ""
+
+# Declaramos user_word, donde el usuario va a ingresar la palabra, despues va a almacenar toda esa palabra con mayusculas (.upper())
+user_word = input("Ingrese la palabra: ").upper()
+
+# Declaracion de una lista, con todas las vocales en mayuscula
+vocales = ["A", "E", "I", "O", "U"]
+
+# Indicar al usuario que ingrese una palabra
+# y asignarla a la variable user_word.
+
+
+# Por cada letra que existe en la palabra definida anteriormente.
+for letter in user_word:
+
+   # Si la letra se encuentra dentro de la lista vocales --> continuar (saltear a la siguiente palabra en vocales)
+   if letter in vocales:
+       continue
+   else: # Else --> agregarle la letra a la variable de palabras sin vocales
+       word_without_vowels += letter
+
+# Escribir en la consola la palabra sin vocales
+print(word_without_vowels)
+
+# Imprimir la palabra asignada a word_without_vowels.
