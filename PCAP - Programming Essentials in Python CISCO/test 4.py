4e389a371be472d0045ea2d1ccda35c8d016609b
egiam <ezegiampaoli@gmail.com>
2022-05-30 14:23:44 -0300

    Update

---
+++ PCAP - Programming Essentials in Python CISCO/test 4.py
@@ -0,0 +1,74 @@
+
+
+from tkinter import Y
+
+
+def fun(x):
+    global y
+    y = x*x
+    return y
+
+fun(2)
+print(y)
+
+# try:
+#     value = "cosas"
+#     print(value/value)
+# except ZeroDivisionError:
+#     print("Division by zero is not allowed")
+# except TypeError:
+#     print("Type Error")
+# except ValueError:
+#     print("Error")
+# except:
+#     print("Hola")
+
+# def func(x):
+#     return 
+
+# print(func(1) + 1)
+
+# def fun(inp = 2, out=3):
+#     return inp * out
+
+# print(fun(out=2))
+
+# tup = (1, 2, 4, 8)
+# tup = tup[1:-1]
+# tup = tup[0]
+# print(tup)
+
+# my_list = ['Mary', 'had', 'a', 'little', 'lamb']
+
+# def my_list(my_list):
+#     del my_list[3]
+#     my_list[3] = 'ram'
+
+# print(my_list(my_list))
+
+# dictionary = {'one': 'two', 'three': 'one', 'two': 'three'}
+# v = dictionary['one']
+
+# for k in range(len(dictionary)):
+#     v = dictionary[v]
+
+# print(v)
+
+# def any():
+#     print(var + 1, end='')
+
+# var = 1
+# any()
+# print(var)
+
+# def f(x):
+#     if x == 0:
+#         return 0
+#     return x + f(x - 1)
+
+# print(f(3))
+
+# numero = None
+# valor = None > numero
+
+# None
