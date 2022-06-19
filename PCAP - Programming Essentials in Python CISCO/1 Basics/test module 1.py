51dcdb6fcce39a88a52747650544752a06895ad1
egiam <ezegiampaoli@gmail.com>
2022-06-19 15:37:24 -0300

    Module 2 U. 1

---
+++ PCAP - Programming Essentials in Python CISCO/1 Basics/test module 1.py
@@ -0,0 +1,115 @@
+
+
+x = 3 % 2
+x = x % 2
+y = 2 % x
+print(y)
+
+# a = 1
+# b = 0
+# a = a ^ b
+# b = a ^ b
+# a = a ^ b
+
+# print(a,b)
+
+# print(1//5+1/5)
+
+# my_list = [x * x for x in range(5)]
+
+# def fun(lst):
+#     del lst[lst[2]]
+#     return lst
+
+# print(fun(my_list))
+
+# def func(a,b):
+#     return b **a
+
+# print(func(b=2,2))
+
+# x = float(2)
+# y = float(4)
+
+# print(y**(1/x))
+
+# dct = {'one': 'two', 'three': 'one', 'two': 'three'}
+# v = dct['three']
+
+# for k in range(len(dct)):
+#     v = dct[v]
+
+# print(v)
+
+# my_list = [1,2]
+
+# for v in range(2):
+#     my_list.insert(-1, my_list[v])
+
+# print(my_list)
+
+# print(0/len("0"))
+
+# foo = (1,2,3)
+# foo.index(0)
+
+# for = 0
+# print = 12
+# in = 12
+# In = 12
+
+# try:
+#     print(5/0)
+#     break
+# except:
+#     print("losiento")
+# except (ValueError, ZeroDivisionError):
+#     print("chau")
+
+# dct = {}
+# dct['1'] = (1,2)
+# dct['2'] = (2,1)
+
+# for x in dct.keys():
+#     print(dct[x][1], end="")
+
+# x = 1
+# y = 2
+# x,y,z = x,x,y
+# z,y,z = x,y,z
+
+# print(x,y,z)
+
+# lst = [[x for x in range(3)] for y in range(3)]
+
+# for r in range(3):
+#     for c in range(3):
+#         if lst[r][c] % 2 != 0:
+#             print("#")
+
+# tup = (1,2,4,8)
+# tup = tup[-2:-1]
+# tup = tup[-1]
+# print(tup)
+
+# lst = [i for i in range(-1,-2)]
+# print(len(lst))
+
+# print(1%2)
+
+# nums = [1, 2, 3]
+# vals = nums
+# del vals[:]
+
+# print(len(nums), len(vals))
+
+# print(1//2)
+
+
+# dd = {"1": "0", "0": "1"}
+# for x in dd.vals():
+#     print(x, end="")
+
+
+
+# 
--- PCAP - Programming Essentials in Python CISCO/Basics/4/main_imc.py
+++
@@ -1,5 +0,0 @@
-﻿def bmi(weight, height):
-    return weight / height ** 2
-
-
-print(bmi(52.5, 1.65))
