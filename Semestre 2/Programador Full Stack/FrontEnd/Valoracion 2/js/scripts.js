f6ccfbae377fd598975c156d22f0cec92e6e2630
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-10-30 11:02:41 -0700

    Evaluacion frontend 2

---
+++ Semestre 2/Programador Full Stack/FrontEnd/Valoracion 2/js/scripts.js
@@ -0,0 +1,50 @@
+let premiaciones = {
+    premio: "Emy 2022",
+
+    tipo: "Serie Dramática",
+
+    nominados: ["Lee Jung-jae", "Bob Odenkirk", "Brian Cox", "Jeremy Strong"],
+
+    ganador: "Lee Jung-jae",
+};
+
+// console.log(premiaciones["nominados"]["Brian Cox"]);
+console.log(premiaciones["nominados"][2]);
+// console.log(premiaciones[nominados][2]);
+// console.log(premiaciones[2][2]);
+
+// let impuesto = 25;
+
+// if (impuesto < 15 && impuesto > 10) {
+
+//
+//     console.log("El porcentaje de impuesto es 25%")
+
+// } else {
+
+//
+//     console.log("El porcentaje de impuesto es 0%")
+
+// }
+
+// let impuesto = 36,
+//     indice = 3,
+//     x = 8,
+//     y = 7;
+
+// if (impuesto > indice && impuesto < y) {
+
+//
+//     console.log("El porcentaje de impuesto es 25%");
+
+// } else if (impuesto > indice && impuesto < x) {
+
+//
+//     console.log("El porcentaje de impuesto es 12%");
+
+// } else {
+
+//
+//     console.log("El porcentaje de impuesto es 0%");
+
+// }
